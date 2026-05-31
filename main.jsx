92fde9a01e3d976fa3545ae53b80a24f0c7ebabb
josephchristianre-cmd <josephchristian.re@gmail.com>
2026-05-31 16:29:02 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Portal from './TMDocPortal.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><Portal/></React.StrictMode>
-)
